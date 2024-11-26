06204b55246ede73dfc613ae2eccafda52722aba
EduardoFigue001 <ed.figueroaf@duocuc.cl>
2024-11-26 20:56:37 -0300

    se actualiza con componentes de android

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { GoogleMapsModule } from '@angular/google-maps'; // Importar GoogleMapsModule
+// Importar GoogleMapsModule
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/pages/services/services.page.spec.ts
+++ src/app/pages/services/services.page.spec.ts
@@ -1,9 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ServicesPage } from './services.page';
+
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ServicesPage } from 'src/app/services/services.page';
 
 describe('ServicesPage', () => {
   let component: ServicesPage;
   let fixture: ComponentFixture<ServicesPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServicesPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Para evitar errores con componentes no declarados
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ServicesPage);
@@ -12,6 +22,6 @@ describe('ServicesPage', () => {
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy();  // Verifica que el componente se haya creado
   });
 });
